2d4dea79f4fe058248526519ae708172faac9b39
Rob Nugen <rob@robnugen.com>
2022-09-13 18:39:42 +0900

    Use ` and $
    required for node to display ${hostname} and ${port}
    
    But when being sent through cat, BASH was trying to execute them so need to escape the execution.
    
    https://unix.stackexchange.com/questions/48392/understanding-backtick

--- bootstrap-home-dir-as-vagrant.sh
+++ bootstrap-home-dir-as-vagrant.sh
@@ -99,7 +99,7 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-  console.log("Server running at http://${hostname}:${port}/");
+  console.log(\`Server running at http://\${hostname}:\${port}/\`);
 });
 EOL
 fi
