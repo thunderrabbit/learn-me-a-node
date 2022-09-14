9eabd47c937cd38f0bf3aff6d6f21c42aa1c8fc6
Rob Nugen <rob@robnugen.com>
2022-09-14 22:35:27 +0900

    Near-blindly copy code to install Postgresql on Vagrant
    https://github.com/berkanyiildirim/postgresql-pagila-vm/blob/master/Vagrant-setup/bootstrap.sh

---
+++ bootstrap-postgresql-as-vagrant.sh
@@ -0,0 +1,106 @@
+# Edit the following to change the name of the database user that will be created:
+APP_DB_USER=gojilla
+APP_DB_PASS=dbpass
+
+# Edit the following to change the name of the database that is created (defaults to the user name)
+APP_DB_NAME=$APP_DB_USER
+
+# Edit the following to change the version of PostgreSQL that is installed
+PG_VERSION=12
+
+###########################################################
+# Changes below this line are probably not necessary
+###########################################################
+print_db_usage () {
+  echo "Your PostgreSQL database has been setup and can be accessed on your local machine on the forwarded port (default: 15432)"
+  echo "  Host: localhost"
+  echo "  Port: 15432 (default value in Vagranfile)"
+  echo "  Database: $APP_DB_NAME"
+  echo "  Username: $APP_DB_USER"
+  echo "  Password: $APP_DB_PASS"
+  echo ""
+  echo "Admin access to postgres user via VM:"
+  echo "  vagrant ssh"
+  echo "  sudo su - postgres"
+  echo ""
+  echo "psql access to app database user via VM:"
+  echo "  vagrant ssh"
+  echo "  sudo su - postgres"
+  echo "  PGUSER=$APP_DB_USER PGPASSWORD=$APP_DB_PASS psql -h localhost $APP_DB_NAME"
+  echo ""
+  echo "Env variable for application development:"
+  echo "  DATABASE_URL=postgresql://$APP_DB_USER:$APP_DB_PASS@localhost:15432/$APP_DB_NAME"
+  echo ""
+  echo "Local command to access the database via psql:"
+  echo "  PGUSER=$APP_DB_USER PGPASSWORD=$APP_DB_PASS psql -h localhost -p 15432 $APP_DB_NAME"
+}
+
+
+PROVISIONED_ON=/etc/vm_provision_on_timestamp
+if [ -f "$PROVISIONED_ON" ]
+then
+  echo "VM was already provisioned at: $(cat $PROVISIONED_ON)"
+  echo "To run system updates manually login via 'vagrant ssh' and run 'apt-get update && apt-get upgrade'"
+  echo ""
+  print_db_usage
+  exit
+fi
+
+# Install the repository RPM:
+yum -y install "https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm"
+
+# Install PostgreSQL:
+yum -y install "postgresql$PG_VERSION-server"
+"/usr/pgsql-$PG_VERSION/bin/postgresql-$PG_VERSION-setup" initdb
+systemctl enable "postgresql-$PG_VERSION.service"
+systemctl start "postgresql-$PG_VERSION.service"
+
+PG_CONF="/var/lib/pgsql/$PG_VERSION/data/postgresql.conf"
+PG_HBA="/var/lib/pgsql/$PG_VERSION/data/pg_hba.conf"
+PG_DIR="/var/lib/pgsql/$PG_VERSION/data"
+
+# Edit postgresql.conf to change listen address to '*':
+sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" "$PG_CONF"
+
+# Append to pg_hba.conf to add password auth:
+echo "host    all             all             all                     md5" >> "$PG_HBA"
+
+# Explicitly set default client_encoding
+echo "client_encoding = utf8" >> "$PG_CONF"
+
+# Restart so that all new config is loaded:
+systemctl restart "postgresql-$PG_VERSION.service"
+
+# Install git and clone pagila database
+yum -y install git
+cd /var/lib/pgsql
+git clone https://github.com/devrimgunduz/pagila.git
+
+cat << EOF | su - postgres -c psql
+-- Create the database user:
+CREATE USER $APP_DB_USER WITH SUPERUSER LOGIN PASSWORD '$APP_DB_PASS';
+
+-- Create the database:
+CREATE DATABASE $APP_DB_NAME WITH OWNER=$APP_DB_USER
+                                  LC_COLLATE='en_US.utf8'
+                                  LC_CTYPE='en_US.utf8'
+                                  ENCODING='UTF8'
+                                  TEMPLATE=template0;
+
+-- Insert data to pagila database
+\c pagila
+\i /var/lib/pgsql/pagila/pagila-schema.sql
+\i /var/lib/pgsql/pagila/pagila-data.sql
+EOF
+
+# Tag the provision time:
+date > "$PROVISIONED_ON"
+
+echo "Successfully created PostgreSQL dev virtual machine."
+echo ""
+print_db_usage
+
+sed -i s/^SELINUX=.*$/SELINUX=disabled/ /etc/selinux/config
+systemctl disable firewalld
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+reboot
