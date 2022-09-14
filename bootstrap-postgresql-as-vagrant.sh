2a43eb0a2ea904fabbad251f5c4fd32cc963a302
Rob Nugen <rob@robnugen.com>
2022-09-14 22:46:26 +0900

    Create urandom password for Postgres

--- bootstrap-postgresql-as-vagrant.sh
+++ bootstrap-postgresql-as-vagrant.sh
@@ -1,6 +1,6 @@
 # Edit the following to change the name of the database user that will be created:
 APP_DB_USER=gojilla
-APP_DB_PASS=dbpass
+APP_DB_PASS=$(< /dev/urandom tr -dc "[:alnum:]" | head -c20)
 
 # Edit the following to change the name of the database that is created (defaults to the user name)
 APP_DB_NAME=$APP_DB_USER
